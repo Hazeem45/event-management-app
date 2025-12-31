570441ffd56607b183ed4b08607490270236f2be
Hazeem <hasyim.fery27@gmail.com>
2025-12-31 17:12:42 +0700

    setup project

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Semina-Event Management API is running",
+  });
+});
+
+export default app;
