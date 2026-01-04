fcd413744e22049191a1fddc8c7a8642192d31fc
Hazeem <hasyim.fery27@gmail.com>
2026-01-04 15:58:58 +0700

    setup project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,10 @@
-import express, { Application, Request, Response } from "express";
+import express from "express";
+import router from "./routes/api";
+import dotenv from "dotenv";
 
-const app: Application = express();
+dotenv.config();
+const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "Semina-Event Management API is running",
-  });
-});
+app.use("/api", router);
 
 export default app;
