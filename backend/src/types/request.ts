18e138b75c6a225e61be1178c7ecc55b1b82ffed
Hazeem <hasyim.fery27@gmail.com>
2026-02-06 13:08:38 +0700

    refactor: request interface

--- backend/src/types/request.ts
+++ backend/src/types/request.ts
@@ -1,6 +1,11 @@
 import { Request } from "express";
 import { AccessTokenPayload, JwtMeta } from "./jwt";
 
-export interface IRequestExtended extends Request {
+export interface IRequestExtended<
+  P = any,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any
+> extends Request<P, ResBody, ReqBody, ReqQuery> {
   user?: AccessTokenPayload & JwtMeta;
 }
